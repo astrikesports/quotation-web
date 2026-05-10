6b73d07d55a140fcce88643449694363db35febf
astrikesports <astrikebackend@gmail.com>
2026-05-10 12:52:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Quotation from "./pages/Quotation";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/quotation" element={<Quotation />} />
       </Routes>
     </BrowserRouter>
