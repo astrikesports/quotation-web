64998ba974f5dbaae6a86f710222798ad033068c
astrikesports <astrikebackend@gmail.com>
2026-05-10 13:30:09 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
+
+import { supabase } from "../supabase";
+
 import AdminDashboard from "./AdminDashboard";
+
 import SalesTeamDashboard from "./SalesTeamDashboard";
 
 export default function Dashboard() {
 
-  const role = localStorage.getItem("role");
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+
+    getUserRole();
+
+  }, []);
+
+  const getUserRole = async () => {
+
+    // GET LOGGED USER
+    const {
+
+      data: { user }
+
+    } = await supabase.auth.getUser();
+
+    if (!user) return;
+
+    // FETCH ROLE
+    const { data } = await supabase
+
+      .from("users")
+
+      .select("role")
+
+      .eq("id", user.id)
+
+      .single();
+
+    setRole(data?.role);
+  };
+
+  if (!role) {
+
+    return <div>Loading...</div>;
+  }
 
   if (role === "admin") {
+
     return <AdminDashboard />;
   }
 
