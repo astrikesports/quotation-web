e0adc9b682745f2419b6ec364085c80f08652fbb
astrikesports <astrikebackend@gmail.com>
2026-05-10 17:09:42 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -612,7 +612,7 @@ export default function AdminDashboard() {
           </div>
 
           {/* SALES PERSONS */}
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
 
             {salesPersons.map(
               (person) => {
