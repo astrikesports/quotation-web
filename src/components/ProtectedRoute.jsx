926bc775e137187e2cb7c095b66513b94cf0deaa
astrikesports <astrikebackend@gmail.com>
2026-05-11 12:18:40 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,4 @@
-import { useEffect, useState } from "react";
-
-import {
-  Navigate,
-  useLocation
-} from "react-router-dom";
-
-import { supabase } from "../supabase";
+import { Navigate } from "react-router-dom";
 
 import LoaderOverlay from "../components/LoaderOverlay";
 
@@ -13,103 +6,23 @@ export default function ProtectedRoute({
   children
 }) {
 
-  const location = useLocation();
-
-  const [loading, setLoading] =
-    useState(true);
-
-  const [isAuthenticated,
-    setIsAuthenticated] =
-    useState(false);
-
-  useEffect(() => {
-
-    checkUser();
-
-  }, []);
-
-  // CHECK LOGIN
-  const checkUser = async () => {
-
-    try {
-
-      // LOCAL USER
-      const localUser =
-        localStorage.getItem(
-          "user"
-        );
-
-      // NO USER
-      if (!localUser) {
-
-        setIsAuthenticated(false);
-
-        setLoading(false);
-
-        return;
-      }
-
-      // CHECK SESSION
-      const {
-        data,
-        error
-      } = await supabase.auth.getSession();
-
-      // SESSION INVALID
-      if (
-        error ||
-        !data?.session
-      ) {
-
-        localStorage.removeItem(
-          "user"
-        );
-
-        setIsAuthenticated(false);
-
-        setLoading(false);
-
-        return;
-      }
-
-      // VALID
-      setIsAuthenticated(true);
-
-      setLoading(false);
-
-    } catch (err) {
-
-      console.log(err);
-
-      setIsAuthenticated(false);
-
-      setLoading(false);
-    }
-
-  };
-
-  // LOADING
-  if (loading) {
-
-    return (
-      <LoaderOverlay text="Checking Access..." />
+  // CHECK LOCAL USER
+  const user =
+    localStorage.getItem(
+      "user"
     );
-  }
 
-  // BLOCK ACCESS
-  if (!isAuthenticated) {
+  // NO LOGIN
+  if (!user) {
 
     return (
       <Navigate
         to="/login"
         replace
-        state={{
-          from: location
-        }}
       />
     );
   }
 
-  // ACCESS GRANTED
+  // ACCESS
   return children;
 }
