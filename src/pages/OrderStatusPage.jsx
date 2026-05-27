c2c254095e675da68918156622165e3f9ef17685
astrikesports <astrikebackend@gmail.com>
2026-05-27 11:28:11 +0530

    Update OrderStatusPage.jsx

--- src/pages/OrderStatusPage.jsx
+++ src/pages/OrderStatusPage.jsx
@@ -390,7 +390,7 @@ export default function OrderStatusPage() {
       )}
 
       {/* TOP CARDS */}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
 
         {/* CONFIRMED */}
         <button
