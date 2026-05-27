5ead121096bbd3824f3218fa8164c5f748b0d817
astrikesports <astrikebackend@gmail.com>
2026-05-27 13:16:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,27 @@ import {
   Routes
 } from "react-router-dom";
 
-import Home from "./pages/Home";
-
-import Login from "./pages/Login";
-
-import Dashboard from "./pages/Dashboard";
-
-import Quotation from "./pages/Quotation";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-
 import {
   Toaster
 } from "react-hot-toast";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Quotation from "./pages/Quotation";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
 
   return (
 
     <BrowserRouter>
+
       <Toaster position="top-right" />
 
       <Routes>
 
-        {/* PUBLIC */}
         <Route
           path="/"
           element={<Home />}
@@ -38,30 +35,21 @@ export default function App() {
           element={<Login />}
         />
 
-        {/* PROTECTED */}
         <Route
           path="/dashboard"
           element={
-
             <ProtectedRoute>
-
               <Dashboard />
-
             </ProtectedRoute>
-
           }
         />
 
         <Route
           path="/quotation"
           element={
-
             <ProtectedRoute>
-
               <Quotation />
-
             </ProtectedRoute>
-
           }
         />
 
@@ -70,4 +58,5 @@ export default function App() {
     </BrowserRouter>
 
   );
+
 }
